a13d149c2c26f9af4167f73bec5abab2395f6026
José Arbey López Martínez <14470369@campeche.tecnm.mx>
2021-06-30 20:59:22 -0500

    Se agregó el componente Manage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
